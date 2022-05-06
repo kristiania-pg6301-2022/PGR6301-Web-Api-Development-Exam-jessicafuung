cb289ce5d9ea731cafb9647b2308686fc1b1c140
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-06 06:41:07 +0200

    test updated

--- server/__tests__/newsApi.test.jsx
+++ server/__tests__/newsApi.test.jsx
@@ -12,6 +12,16 @@ const mongoClient = new MongoClient(
   "mongodb+srv://jessicafuung:stress123ball@cluster0.2m1ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
 
+function randomString(length) {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return result;
+}
+
 beforeAll(async () => {
   await mongoClient.connect();
   const database = mongoClient.db("unit_tests");
@@ -25,7 +35,7 @@ afterAll(() => {
 describe("news api test suit", () => {
   it("test article", async () => {
     const author = "Andre Persson";
-    const title = "Solsystemet";
+    const title = randomString(6);
 
     await request(app).post("/api/news").send({ author, title }).expect(200);
 
